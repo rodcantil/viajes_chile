2bf8500f2cb4639e285268dcb2bee330082ae01c
Rodolfo Cantillana <rodolfo.cantillana@gmail.com>
2024-07-24 03:26:25 -0400

    avance 5 Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,24 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 // tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$('#enviarcorreo').click(function () {
+    alert("Su correo fue enviado exitosamente");
+    
+});
+
+$(viajeschile,destacados,quienesomos).click(function() {
+    $(this).css({'color': 'red'});
+});
+
+$(destacados).click(function() {
+    $(this).css({'color': 'red'});
+});
+
+$(quienesomos).click(function() {
+    $(this).css({'color': 'red'});
+});
+
+$(contacto).click(function() {
+    $(this).css({'color': 'red'});
+});
